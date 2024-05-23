f557d00ebbac520f52dd75687bb6bee1becfc46a
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-05-23 18:06:00 -0300

    :bricks: config: Configurações Iniciais

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+export default function StoreLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+}
