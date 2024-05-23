2d832feebd84ee050fd8edb6e5e31e796def84a4
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-05-23 18:05:17 -0300

    :sparkles: feat: criando estrutura da page Search

---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,6 @@
+
+export default function SearchLoading() {
+  return (
+    <div>Carregando busca...</div>
+  )
+}
---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,4 @@
+export default async function Search() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return <div>Page Search</div>;
+}
