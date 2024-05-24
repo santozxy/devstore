53ac8c853f6aeb5de2043a2fab4aa73e413386c1
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-05-24 00:26:34 -0300

    :sparkles: feat: Adicionando componente Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { Search } from "lucide-react";
+import Image from "next/image";
+import { Search, ShoppingCart } from "lucide-react";
 export default function Header() {
   return (
     <header className="flex items-center justify-between">
@@ -19,7 +20,23 @@ export default function Header() {
           />
         </form>
       </div>
-      <div className="flex items-center gap-4"></div>
+      <div className="flex items-center gap-4">
+        <Link href="/cart" className="flex items-center gap-2 hover:underline">
+          <ShoppingCart className="w-6 h-6" />
+          <span className="text-sm">Cart (0)</span>
+        </Link>
+        <div className="w-px h-4 bg-zinc-700" />
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+          <Image
+            src="https://github.com/santozxy.png"
+            className="w-6 h-6 rounded-full"
+            width={24}
+            height={24}
+            alt="avatar"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
