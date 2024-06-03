eea69e5980246803022023afb3863d94e14e8a0f
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-03 16:14:41 -0300

    :sparkles: feat: Add componente Loading Skeleton

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,8 +1,10 @@
-export default  function HomeLoading() {
-  
+import { Skeleton } from "@/components/skeleton";
+export default function HomeLoading() {
   return (
-    <main>
-      <h1>Loading...</h1>
-    </main>
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[860px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
   );
 }
