20f68df41c0e212e13183d4b9b08e84f90b1e53c
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-06-03 20:56:12 -0300

    :sparkles: feat: Criando contexto do carrinho de compras

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/header";
+import { CartProvider } from "@/context/cart-context";
 
 export default function StoreLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-      <Header />
-      <main>{children}</main>
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+        <Header />
+        <main>{children}</main>
+      </div>
+    </CartProvider>
   );
 }
---
+++ src/context/cart-context.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { ReactNode, createContext, useContext, useState } from "react";
+
+interface CartItem {
+  product: string;
+  quantity: number;
+}
+
+interface CartContext {
+  items: CartItem[];
+  addToCart: (productId: string) => void;
+}
+const CartContext = createContext({} as CartContext);
+export function CartProvider({ children }: { children: React.ReactNode }) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function addToCart(productId: string) {
+    setCartItems((items) => {
+      const productInCart = items.some((item) => item.product === productId);
+      if (productInCart) {
+        return items.map((item) => {
+          if (item.product === productId) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        });
+      }
+      return [...items, { product: productId, quantity: 1 }];
+    });
+  }
+  return (
+    <CartContext.Provider value={{ items: cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+export const useCart = () => {
+  useContext(CartContext);
+};
