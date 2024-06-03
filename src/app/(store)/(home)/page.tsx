ca49e2620498c5fad54feecd3a313017a5120664
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-06-03 16:14:49 -0300

    :sparkles: feat: Add config metadados

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,28 +1,37 @@
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured");
+  const response = await api("/products/featured", {
+    next: {
+      revalidate: 60 * 60,
+    },
+  });
   const products = await response.json();
   return products;
 }
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default async function Home() {
   const [highlightProduct, ...otherProducts] = await getFeaturedProducts();
   return (
     <main className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${highlightProduct.slug}`}
+        href={`/product/${highlightProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex rounded-lg bg-zinc-900 overflow-hidden justify-center items-start"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-300 ease-in-out"
           src="/moletom-never-stop-learning.png"
           alt="Moletom Never Stop Learning"
-          width={920}
-          height={920}
+          width={820}
+          height={820}
           quality={100}
         />
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
@@ -39,7 +48,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/products/${product.slug}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 flex rounded-lg bg-zinc-900 overflow-hidden justify-center items-start"
         >
           <Image
